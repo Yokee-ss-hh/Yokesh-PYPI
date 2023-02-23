13abdcbad6930be4a493c82fb8c1427e4f2a85b2
LTPB\U6069615 <Yokesh.Bollineni@Clarivate.com>
2023-02-23 19:39:40 +0530

    Version 2.0.2 code commit

--- PyMolecules/__init__.py
+++ PyMolecules/__init__.py
@@ -2,5 +2,5 @@ from PyMolecules import SingleLinkedList, DoubleLinkedList,SingleCircularLinkedL
 
 
 __author__ = "Yokesh Bollineni"
-__version__ = "2.0.1"
-__what_is_new__ = "Added SCLL class, Bug fix in size() method of DLL class"
+__version__ = "2.0.2"
+__what_is_new__ = "Removal of junk code in the version 2.0.1"
